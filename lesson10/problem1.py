ee261df7e54dbe8f53dfa6602edb482c81dabc6c
chickfilangel <43784681+chickfilangel@users.noreply.github.com>
2018-12-18 09:55:13 -0500

    Add files via upload

---
+++ lesson10/problem1.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Life Decision Bot')
+print()
+rain = input('Is it raining out? ')
+if rain == 'Yes':
+	print('Stay at home!')
+	print('Maybe binge watch a new show on Netflix? ')
+else:
+	print('Go out and do something!')
+	print('Maybe go on a hike!')
+print('-'*65)
